5a2598c47b14b25d7910e57ccbcd6ba89ce9348d
wangkemiao <wangkemiao@zhihu.com>
2017-08-21 18:47:29 +0800

    initliaze normal tags

--- index.js
+++ index.js
@@ -4,4 +4,9 @@
  * @author Leo Wang(leowang721@gmail.com)
  */
 
-export h1 from './lib/h1'
+export Body from './lib/Body'
+export H1 from './lib/H1'
+export H2 from './lib/H2'
+export H3 from './lib/H3'
+export H4 from './lib/H4'
+export styles from './lib/styles'
