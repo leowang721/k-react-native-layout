7b9a672e9e3574d650551e8c70c416cd8922642b
wangkemiao <wangkemiao@zhihu.com>
2017-08-22 17:24:30 +0800

    make Body a whole Component with header,content,footer

--- index.js
+++ index.js
@@ -5,8 +5,11 @@
  */
 
 export Body from './lib/Body'
+export Section from './lib/Section'
+export styles from './lib/styles'
 export H1 from './lib/H1'
 export H2 from './lib/H2'
 export H3 from './lib/H3'
 export H4 from './lib/H4'
-export styles from './lib/styles'
+export H5 from './lib/H5'
+export H6 from './lib/H6'
